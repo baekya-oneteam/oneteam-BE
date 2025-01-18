34de9562e3d841f1af90ff027ba1107afd948798
shinminkyoung1 <dottiee99@naver.com>
2025-01-18 17:34:05 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/hansotbap/hansotbap_BE/HansotbapBeApplication.java
@@ -0,0 +1,13 @@
+package hansotbap.hansotbap_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HansotbapBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HansotbapBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hansotbap/hansotbap_BE/HansotbapBeApplicationTests.java
@@ -0,0 +1,13 @@
+package hansotbap.hansotbap_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansotbapBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
